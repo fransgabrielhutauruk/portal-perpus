2a4fca70aafc0122ccbfac4573aa01db9d497612
Blackjack Colver Voltaire <nandahe420@gmail.com>
2025-11-19 10:15:13 +0700

    Berhasil membuat tabel mst_periode

--- database/migrations/2025_11_19_025252_create_mst_periode.php
+++ database/migrations/2025_11_19_025252_create_mst_periode.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('mst_periode', function (Blueprint $table) {
-            $table->id();
+            $table->id('periode_id');
             $table->timestamps();
             $table->string("nama_periode");
             $table->date("tanggal_mulai");
