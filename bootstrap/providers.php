62b1aed982abc9e4374ab0ce3e83cc60624d6a78
Frans Gabriel Hutauruk <frans22si@mahasiswa.pcr.ac.id>
2026-07-03 11:11:50 +0700

    fx yajra error

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,6 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Frontend\DirectivesProvider::class,
     App\Providers\ViewServiceProvider::class,
+    Yajra\DataTables\DataTablesServiceProvider::class,
+    Vinkla\Hashids\HashidsServiceProvider::class,
 ];
